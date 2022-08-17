98008fd6e67c5362a411093e09037b11c117610a
Sanish Thapa <thapasanish73@gmail.com>
2022-08-17 19:42:56 +0545

    .

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Food Order</title>
+    <title>Manage Admin</title>
 
 
     <!-- link -->
@@ -36,28 +36,8 @@
     <!-- main content section starts -->
     <div class="main-content">
         <div class="wrapper">
-            <h1>Dashboard</h1>
-            <div class="col-4 text-center">
-                <h1>5</h1>
-                <br>
-                Categories
-            </div>
-            <div class="col-4 text-center">
-                <h1>5</h1>
-                <br>
-                Categories
-            </div>
-            <div class="col-4 text-center">
-                <h1>5</h1>
-                <br>
-                Categories
-            </div>
-            <div class="col-4 text-center">
-                <h1>5</h1>
-                <br>
-                Categories
-            </div>
-            <div class="clear-fix"></div>
+            <h1>Manage Admin</h1>
+           
         </div>
 
     </div>
