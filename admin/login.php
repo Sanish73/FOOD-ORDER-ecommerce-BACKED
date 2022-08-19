c7a912a4c0f0a6a5926498e98a4e6ca847249595
Sanish Thapa <thapasanish73@gmail.com>
2022-08-19 17:14:25 +0545

    .

--- admin/login.php
+++ admin/login.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login - Food Order System</title>
+    <!-- -----link------- -->
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
 
+    <div class="login">
+       <h1>Login</h1>
 
+       <p>Created By- Sanish Thapa</p>
+    </div>
 </body>
