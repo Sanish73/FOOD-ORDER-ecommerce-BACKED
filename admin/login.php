f4a941d5aa5f51668a9faa809732b345190f1f8a
Sanish Thapa <thapasanish73@gmail.com>
2022-08-19 18:04:18 +0545

    .

--- admin/login.php
+++ admin/login.php
@@ -15,7 +15,7 @@
 <body>
 
     <div class="login">
-        <h1 class="text-center">Login</h1><br>
+        <h1 class="text-center secondary-text">Login</h1><br>
 
         <?php
         if (isset($_SESSION['login'])) {
@@ -25,14 +25,16 @@
         ?><br><br>
 
         <form action="" method="POST" class="text-center">
-            <label>UserName:</label><br>
+            <label class="bold">UserName:</label><br>
             <input type="text" name="username" placeholder="Enter Username."><br><br>
-            <label>Password:</label><br><br>
+            <label class="bold">Password:</label><br><br>
             <input type="password" name="password" placeholder="Enter Password."><br><br>
             <input type="submit" name="submit" value="Submit" class="btn-primary">
         </form>
         <br><br>
-        <p>Created By- <a href="#">Sanish Thapa</a></p>
+        <h2>
+            <p class="text-center bold primary-text">Food Order System</a></p>
+        </h2>
     </div>
 </body>
 
@@ -43,22 +45,26 @@ if (isset($_POST['submit'])) {
     $userName = $_POST['username'];
     $passWord = md5($_POST['password']);
 
-    $sql = "SELECT * FROM  tbl_admin where username = '$userName' AND password = '$passWord'";
+    if (!empty($userName) && !empty($passWord)) {
+        $sql = "SELECT * FROM  tbl_admin where username = '$userName' 
+              AND
+         password = '$passWord'";
 
-    $qry = mysqli_query($conn, $sql) or die("login query  porblem");
+        $qry = mysqli_query($conn, $sql) or die("login query  porblem");
 
-    // lets check user exist or not by count
-    $count = mysqli_num_rows($qry);
+        // lets check user exist or not by count
+        $count = mysqli_num_rows($qry);
 
-    if ($count == 1) {
-        //user is available and login success
-        echo ("com");
-    } else {
-        //user is not available and login is not success
-        // give message here
-        $_SESSION['login'] = "<div class='text-center'>Failed To Login!!!</div>";
-        //after added we redirect to back page so
-        header('location:' . SITEURL . 'admin/login.php');
+        if ($count == 1) {
+            //user is available and login success
+            echo ("com");
+        } else {
+            //user is not available and login is not success
+            // give message here
+            $_SESSION['login'] = "<div class='text-center primary-text'>Failed To Login!!!</div>";
+            //after added we redirect to back page so
+            header('location:' . SITEURL . 'admin/login.php');
+        }
     }
 }
 
