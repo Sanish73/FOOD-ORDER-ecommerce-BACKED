aab8d049f7dd2f59976ccf36b732ebd91eaf19a3
Sanish Thapa <thapasanish73@gmail.com>
2022-08-19 17:54:08 +0545

    login

--- admin/login.php
+++ admin/login.php
@@ -1,5 +1,8 @@
+<?php include('../config/constant.php');  ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,31 +11,55 @@
     <!-- -----link------- -->
     <link rel="stylesheet" href="../css/admin.css">
 </head>
+
 <body>
 
     <div class="login">
-       <h1 class="text-center">Login</h1><br><br>
+        <h1 class="text-center">Login</h1><br>
 
-       <form action="" class="text-center">
-        <label>UserName:</label><br>
-        <input type="text" name="username" placeholder="Enter Username." ><br><br>
-        <label>Password:</label><br><br>
-        <input type="password" name="password" placeholder="Enter Password." ><br><br>
-    <input type="submit" name="submit" value="Submit" class="btn-primary">
-       </form>
+        <?php
+        if (isset($_SESSION['login'])) {
+            echo $_SESSION['login'];
+            session_unset();
+        }
+        ?><br><br>
+
+        <form action="" method="POST" class="text-center">
+            <label>UserName:</label><br>
+            <input type="text" name="username" placeholder="Enter Username."><br><br>
+            <label>Password:</label><br><br>
+            <input type="password" name="password" placeholder="Enter Password."><br><br>
+            <input type="submit" name="submit" value="Submit" class="btn-primary">
+        </form>
         <br><br>
-       <p>Created By- <a href="#">Sanish Thapa</a></p>
+        <p>Created By- <a href="#">Sanish Thapa</a></p>
     </div>
 </body>
 
 <!-- ------------------PHP---------------- -->
 <?php
 
-    if(isset($_POST['submit'])){
-        $userName = $_POST['username'];
-        $passWord = $_POST['password'];
+if (isset($_POST['submit'])) {
+    $userName = $_POST['username'];
+    $passWord = md5($_POST['password']);
 
-        
+    $sql = "SELECT * FROM  tbl_admin where username = '$userName' AND password = '$passWord'";
+
+    $qry = mysqli_query($conn, $sql) or die("login query  porblem");
+
+    // lets check user exist or not by count
+    $count = mysqli_num_rows($qry);
+
+    if ($count == 1) {
+        //user is available and login success
+        echo ("com");
+    } else {
+        //user is not available and login is not success
+        // give message here
+        $_SESSION['login'] = "<div class='text-center'>Failed To Login!!!</div>";
+        //after added we redirect to back page so
+        header('location:' . SITEURL . 'admin/login.php');
     }
+}
 
 ?>
