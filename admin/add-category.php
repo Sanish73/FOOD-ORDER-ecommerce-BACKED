30c5ee22c928fe751277bc14d933612762d74c90
Sanish Thapa <thapasanish73@gmail.com>
2022-08-20 19:34:05 +0545

    add-category

---
+++ admin/add-category.php
@@ -0,0 +1,22 @@
+<?php include('partials/menu.php');
+include('partials/login-check.php');
+?>
+<!-- menu section end -->
+
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Add Admin</h1>
+        <br><br>
+        <form name="login" action="" method="POST" onsubmit="validation()">
+        </form>
+    </div>
+</div>
+
+
+
+
+
+
+<!-- footer section starts -->
+<?php include('partials/footer.php') ?>
