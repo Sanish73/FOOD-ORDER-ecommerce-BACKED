863a7d8011bb67e25d612374a559e0229b3aa6cd
Sanish Thapa <thapasanish73@gmail.com>
2022-08-20 19:55:42 +0545

    .

--- admin/add-category.php
+++ admin/add-category.php
@@ -10,8 +10,48 @@ include('partials/login-check.php');
         <br><br>
 
         <form name="login" action="" method="POST" onsubmit="validation()">
+            <table class="tbl-30">
+                <tr>
+                    <td>Title:</td>
 
-        
+                    <td>
+                        <input type="text" name="title" placeholder="Category title">
+
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Featured:</td>
+
+                    <td>
+                        <input type="radio" name="featured" value="Yes">Yes
+                        <input type="radio" name="featured" value="No">No
+
+
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Active:</td>
+
+                    <td>
+                        <input type="radio" name="active" value="Yes">Yes
+                        <input type="radio" name="active" value="No">No
+
+
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="submit" value="Add Category" class="btn-secondary">
+                    </td>
+                </tr>
+
+
+
+            </table>
+
         </form>
     </div>
 </div>
@@ -23,3 +63,37 @@ include('partials/login-check.php');
 
 <!-- footer section starts -->
 <?php include('partials/footer.php') ?>
+
+
+<!-- -----------------php for  uploading data in database------------------ -->
+
+
+
+<?php
+
+    if(isset($_POST['submit'])){
+    //    1.getting the value from form
+    $title = $_POST['title'];
+    
+    // for radio button of featured we have to check whether the button is selected or not .
+    if(isset($_POST['featured'])){
+        // getting the vallue from form
+        $featured = $_POST['featured'];
+        
+
+    }else{
+        $featured = "NO Featured" ;
+    }
+
+    // for radio button 0f active we have to check whether the button is selected or not .
+    if(isset($_POST['active'])){
+        // getting the vallue from form
+        $active = $_POST['active'];
+        
+
+    }else{
+        $active = "NO Active" ;
+    }
+    }
+
+?>
