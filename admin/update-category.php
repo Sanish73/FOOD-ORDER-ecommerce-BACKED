e92ceb0b59ba3eae77d2fef8af174bfc79156857
Sanish Thapa <thapasanish73@gmail.com>
2022-08-21 18:40:13 +0545

    .

---
+++ admin/update-category.php
@@ -0,0 +1,21 @@
+<?php include('partials/menu.php'); ?>
+<!-- menu section end -->
+
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Update Categoty</h1>
+        <br><br </div>
+    </div>
+
+
+
+    <?php
+
+
+    // echo "hi";
+    ?>
+
+
+    <!-- footer section starts -->
+    <?php include('partials/footer.php') ?>
