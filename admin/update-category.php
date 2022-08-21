609693006bd4cf4b39901d22c4917b4fe379b48b
Sanish Thapa <thapasanish73@gmail.com>
2022-08-21 18:50:07 +0545

    s

--- admin/update-category.php
+++ admin/update-category.php
@@ -5,17 +5,67 @@
 <div class="main-content">
     <div class="wrapper">
         <h1>Update Categoty</h1>
-        <br><br </div>
+        <br><br>
+        <!-- ---------------------php for data ------------------- -->
+        <?php
+
+
+        // echo "hi";
+        ?>
+        <!-- ------------------php closed------------------------------ -->
+
+        <form action="" method="POST" enctype="multipart/form-data">
+            <table class="tbl-30">
+                <tr>
+                    <td>Title:</td>
+                    <td>
+                        <input type="text" name="title" value="<?php echo $FullName; ?>">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Current Image:</td>
+                    <td>
+                        <!-- image will be displayed here -->
+                        <!-- <input type="text" name="username" value="<?php //echo $UserName; ?>"> -->
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>New Image:</td>
+                    <td>
+                        <!-- image will be displayed here -->
+                        <input type="file" name="image" >
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Featured:</td>
+                    <td>
+                        <input type="radio" name="featured" value="no">NO
+                        <input type="radio" name="featured" value="yes">YES
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Action:</td>
+                    <td>
+                    <input type="radio" name="featured" value="no">No
+                        <input type="radio" name="featured" value="yes">Yes
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <input type="submit" name="submit" value="Update Category" class="btn-secondary">
+                    </td>
+                </tr>
+            </table>
+
+        </form>
     </div>
+</div>
 
 
-
-    <?php
-
-
-    // echo "hi";
-    ?>
-
-
-    <!-- footer section starts -->
-    <?php include('partials/footer.php') ?>
+<!-- footer section starts -->
+<?php include('partials/footer.php') ?>
