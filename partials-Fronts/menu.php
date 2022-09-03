645cf6fb6afc544b8789f4e89cc964ffd89133cf
Sanish Thapa <thapasanish73@gmail.com>
2022-09-03 12:22:03 +0545

    ljlk

---
+++ partials-Fronts/menu.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food-order</title>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+</head>
+
+<body>
+
+    <!-- -----------------navbar starts ------------------------  -->
+    <section class="navbar">
+        <div class="container">
+            <!------ <h1>navbar</h1> --------->
+            <div class="logo">
+                <a href="#"><img src="images/logo.png" alt="Resturent logo" class="img-responsive"></a>
+            </div>
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Food</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+
+
+    </section>
+    <!-- -----------------navbar Ends ------------------------  -->
+
