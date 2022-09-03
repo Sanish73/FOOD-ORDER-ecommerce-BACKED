aaa9cf12c384ae88516f155d99d814bbad5b0c89
Sanish Thapa <thapasanish73@gmail.com>
2022-09-03 16:19:53 +0545

    fsdaf

---
+++ food-search.php
@@ -0,0 +1,128 @@
+<?php include('partials-Fronts/menu.php') ?>
+
+<!-- -----------------Foood Search starts ------------------------  -->
+<section class="food-search text-center">
+        <div class="container">
+            <!-- <h1>Food search</h1> -->
+            <form action="">
+                <input type="search" name="search" placeholder="Search For Food">
+                <input type="submit" name="submit" value="Search" class="btn btn-primary">
+            </form>
+
+        </div>
+
+    </section>
+    <!-- -----------------Foood Search Ends ------------------------  -->
+
+
+  <!-- -----------------food-menu starts ------------------------  -->
+  <section class="food-menu">
+        <div class="container">
+            <h1 class="text-center">Special Food</h1>
+
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    ` <img src="images/menu-pizza.jpg" alt="Chicken Hawain Pizza" class="img-curve img-responsive">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Chicken Pizza</h4>
+                    <p class="food-price">$1.3</p>
+                    <p class="food-desc">this food is one of the kind </p>
+                    <br>
+                    <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+            <!-- ------------------------- -->
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    ` <img src="images/menu-burger.jpg" alt="Chicken Hawain Burger" class="img-curve img-responsive">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Burger</h4>
+                    <p class="food-price">$3</p>
+                    <p class="food-desc">this food is one of the kind </p>
+                    <br>
+                    <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+            <!-- ------------------------------ -->
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    ` <img src="images/i01_chevanon.jpg" alt="i01_chevanon" class="img-curve img-responsive">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Chevanon</h4>
+                    <p class="food-price">$5</p>
+                    <p class="food-desc">this food is one of the kind </p>
+                    <br>
+                    <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+            <!-- ---------------------------  -->
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    ` <img src="images/pexels-cats-coming-955137.jpg" alt="Yamari" class="img-curve img-responsive">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Yamari</h4>
+                    <p class="food-price">$3.5</p>
+                    <p class="food-desc">this food is one of the kind </p>
+                    <br>
+                    <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+            <!-- ----------------------------------  -->
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    ` <img src="images/fried.jpg" alt="Fried Rice" class="img-curve img-responsive">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Fried Rice</h4>
+                    <p class="food-price">$1</p>
+                    <p class="food-desc">this food is one of the kind </p>
+                    <br>
+                    <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+            <!-- ---------------------------  -->
+            <div class="food-menu-box">
+                <div class="food-menu-img">
+                    ` <img src="images/menu-momo.jpg" alt="Momo" class="img-curve img-responsive">
+                </div>
+
+                <div class="food-menu-desc">
+                    <h4>Chicken Momo</h4>
+                    <p class="food-price">$1.5</p>
+                    <p class="food-desc">this food is one of the kind </p>
+                    <br>
+                    <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+
+
+            <div class="clearfix"></div>
+        </div>
+
+    </section>
+    <!-- -----------------food-menu ENds ------------------------  -->
+
+
+
+<?php include('partials-Fronts/footer.php'); ?>
---
+++ order.php
@@ -0,0 +1,56 @@
+<?php include('partials-Fronts/menu.php') ?>
+
+<!-- -----------------Foood Search starts ------------------------  -->
+<section class="food-search">
+    <h1  class=" text-center">Fill this form to Order</h1>
+
+    <form class="order-form" action="" method="POST">
+        <fieldset class="order-fieldset">
+            <legend >Selected Food</legend>
+            <div class="food-menu-box order-menu-box">
+                <div class="food-menu-img order-menu-img">
+                    ` <img src="images/i01_chevanon.jpg" alt="i01_chevanon" class="img-curve img-responsive">
+                </div>
+
+                <div class="food-menu-desc order-menu-desc">
+                    <h4>Chevanon</h4>
+                    <p class="food-price">$5</p>
+                    <h4>Quantity:</h4>
+                    <!-- <p class="food-desc">this food is one of the kind </p> -->
+                    <br>
+                    <!-- <a href="#" class="btn btn-primary">Order Now</a> -->
+                    <input type="number" name="quantity" id="quantity">
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+        </fieldset>
+    </form>
+
+    <form class="order-form" action="" method="POST">
+        <fieldset class="order-fieldset">
+            <legend >Delivery Details</legend>
+
+           <label for="">Full Name:</label>
+           <input type="text" name="fullname" placeholder="E.g Sanish Thapa"><br>
+
+           <label for="">Phone Number:</label>
+           <input type="text" name="fullname" placeholder="E.g 984******"><br>
+           
+           <label for="">Email:</label>
+           <input type="email" name="fullname" placeholder="E.g food@gmail.com"><br>
+
+           <label for="">Address:</label>
+           <textarea name="description" cols="30" rows="5" placeholder="E.g food@gmail.com"></textarea>
+          
+           <input type="submit" name="submit" value="submit">
+        </fieldset>
+    </form>
+   
+</section>
+<!-- -----------------Foood Search Ends ------------------------  -->
+
+
+
+
+<?php include('partials-Fronts/footer.php'); ?>
